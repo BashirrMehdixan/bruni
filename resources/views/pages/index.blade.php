17249a85967e98a1bb176b7a3cab46a168471328
bashirr <bashir.mehdikhan@gmail.com>
2025-02-09 23:42:20 +0400

    video added to work bg

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.design')
 @section('content')
-    <section class="h-screen flex flex-col items-center justify-center gap-8">
-        <h1 class="text-4xl lg:text-[120px] leading-none text-main font-bold text-center uppercase">
+    <section class="relative h-screen flex flex-col items-center justify-center gap-8">
+        <video autoplay muted loop src="{{ asset('front/videos/httpswwwyoutubecomwatchvldj14haugrw.mp4') }}" class="size-full absolute z-0"></video>
+        <h1 class="text-4xl lg:text-[120px] leading-none text-main font-bold text-center uppercase z-10">
             Ellada Ismayil
         </h1>
-        <a href="{{ route('works.index') }}" class="btn btn_main_outline">Enter</a>
+        <a href="{{ route('works.index') }}" class="btn btn_main_outline hover:text-white z-10">Enter</a>
     </section>
 @endsection
