9933ad9c47eef169eebfafa1906d3163afe535ac
bashirr <bashir.mehdikhan@gmail.com>
2025-02-14 12:20:24 +0400

    work page changed

--- resources/views/pages/works/index.blade.php
+++ resources/views/pages/works/index.blade.php
@@ -10,210 +10,210 @@
                     <a href="{{ asset('front/image/works/work_4.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/work_4.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528151-ZPBARTCV-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528151-ZPBARTCV-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528157-TLLUUNJK-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528157-TLLUUNJK-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528163-VFPXPBUD-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528163-VFPXPBUD-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528909-LZCGYKSS-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528909-LZCGYKSS-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8529267-HSC00923-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8529267-HSC00923-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9030539-WKBNJOYS-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9030539-WKBNJOYS-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9034105-BCVJUGMO-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9034105-BCVJUGMO-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080421-AVIPHZIU-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080421-AVIPHZIU-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080443-WAQBUIJR-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080443-WAQBUIJR-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080479-HNMYSPLO-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080479-HNMYSPLO-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9246677-LQJEVUFV-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9246677-LQJEVUFV-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9248333-NDLAIZZL-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9248333-NDLAIZZL-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/10222531-CSNTKOWY-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/10222531-CSNTKOWY-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/10222653-OYCEZKOK-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/10222653-OYCEZKOK-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/work_4.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/work_4.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528151-ZPBARTCV-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528151-ZPBARTCV-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528157-TLLUUNJK-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528157-TLLUUNJK-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528163-VFPXPBUD-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528163-VFPXPBUD-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8528909-LZCGYKSS-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8528909-LZCGYKSS-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/8529267-HSC00923-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/8529267-HSC00923-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9030539-WKBNJOYS-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9030539-WKBNJOYS-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9034105-BCVJUGMO-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9034105-BCVJUGMO-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080421-AVIPHZIU-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080421-AVIPHZIU-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080443-WAQBUIJR-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080443-WAQBUIJR-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9080479-HNMYSPLO-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9080479-HNMYSPLO-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9246677-LQJEVUFV-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9246677-LQJEVUFV-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/9248333-NDLAIZZL-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/9248333-NDLAIZZL-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/10222531-CSNTKOWY-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/10222531-CSNTKOWY-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
                 <div class="h-[220px]">
                     <a href="{{ asset('front/image/works/10222653-OYCEZKOK-6.jpg') }}" data-fancybox="gallery"
                        class="block size-full">
                         <img src="{{ asset('front/image/works/10222653-OYCEZKOK-6.jpg') }}" alt="Work 1"
-                             class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">
+                             class="size-full object-cover transition duration-500">
                     </a>
                 </div>
 {{--                @isset($works)--}}
@@ -223,7 +223,7 @@
 {{--                               class="block size-full">--}}
 {{--                                <img src="{{ asset('storage/'.$work->thumbnail) }}" title="{{ $work->title }}"--}}
 {{--                                     alt="{{ $work->title }}"--}}
-{{--                                     class="size-full object-cover grayscale transition duration-500 hover:grayscale-0">--}}
+{{--                                     class="size-full object-cover transition duration-500">--}}
 {{--                            </a>--}}
 {{--                        </div>--}}
 {{--                    @endforeach--}}
